d734663edb7005b5c0f21e6c778002ddd9444197
marina makarova <marina-r-0308@mail.ru>
2022-12-05 18:37:41 +0500

    вторая версия

--- samples/admin.py
+++ samples/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 from .models import Samples
 
 
 @admin.register(Samples)
 class SamplesAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('sample_name', 'get_html_xrr')
+
+    def get_html_xrr(self, object):
+        if object.fig_xrr:
+            return mark_safe(f"<img src='{object.fig_xrr.url}' width=100")
--- samples/models.py
+++ samples/models.py
@@ -9,7 +9,7 @@ class BaseUuid(models.Model):
 # Create your models here.
 class Samples(BaseUuid):
    sample_name = models.CharField("Название образца", max_length=100)
-   formula = models.TextField
+   formula = models.CharField(blank=True, null=True, max_length=50)
    fabrication = models.TextField(blank=True)
    fig_xrr = models.ImageField(upload_to="XRR_data/", default='XRR_data/')
    fig_xrd = models.ImageField(upload_to="XRD_data/", default='XRD_data/')
