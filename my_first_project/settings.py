bf5d4f0799c80e02122e71d02e81e4d69caa7036
marina makarova <marina-r-0308@mail.ru>
2022-12-05 18:02:17 +0500

    Первая версия полей

--- my_first_project/settings.py
+++ my_first_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-q5a%zscjs1^4&_u1t*+&z)c77==g*8@vg33sd5p63piy@3sd9s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -110,9 +111,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = '/static/'
 STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'figs')
+MEDIA_URL = '/figs/'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- samples/models.py
+++ samples/models.py
@@ -3,8 +3,13 @@ from uuid import uuid4
 
 
 class BaseUuid(models.Model):
-	id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
+   id = models.UUIDField(primary_key=True, default=uuid4(), editable=False)
+
 
 # Create your models here.
 class Samples(BaseUuid):
-    sample_name = models.CharField("Название образца", max_length=100)
+   sample_name = models.CharField("Название образца", max_length=100)
+   formula = models.TextField
+   fabrication = models.TextField(blank=True)
+   fig_xrr = models.ImageField(upload_to="XRR_data/", default='XRR_data/')
+   fig_xrd = models.ImageField(upload_to="XRD_data/", default='XRD_data/')
